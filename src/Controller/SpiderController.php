26ce6051837fcb93ca2fcbef0a04033b4373286e
Tom Ferreol <ferreolt@gmail.com>
2017-12-14 09:32:08 +0100

    First commit

---
+++ src/Controller/SpiderController.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Spiders;
+
+class SpiderController extends Controller
+{
+
+  /**
+     * @Route("/", name="default")
+     */
+  public function default()
+  {
+    /*$repository = $this->getDoctrine()->getRepository(Spiders::class);
+        $spiders=$repository->findAll();*/
+
+        return $this->redirectToRoute('spider');
+  }
+
+    /**
+     * @Route("/spider", name="spider")
+     */
+    public function index()
+    {
+      $repository = $this->getDoctrine()->getRepository(Spiders::class);
+      $spiders=$repository->findAll();
+      return $this->render('spider.html.twig', array(
+    'spiders' => $spiders));
+    }
+
+    /**
+     * @Route("/spider/add", name="spider_add")
+     */
+    public function addAction(Request $request){
+      $spider=new Spiders(); 
+        $form = $this->createFormBuilder($spider)
+            ->add('Name', TextType::class)
+            ->add('Price', NumberType::class)
+            ->add('Save', SubmitType::class, array('label' => 'Add Spider'))
+            ->getForm();
+
+             $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // $form->getData() holds the submitted values
+        // but, the original `$task` variable has also been updated
+        $spider = $form->getData();
+        $em = $this->getDoctrine()->getManager();
+         $em->persist($spider);
+         $em->flush();
+         return $this->redirectToRoute('spider');
+       }
+
+            return $this->render('spider/add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+     /**
+     * @Route("/spider/remove/{id}", name="spider_remove")
+     */
+    public function removeAction($id){
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: index(EntityManagerInterface $em)
+        $em = $this->getDoctrine()->getManager();
+
+        $spider = $em->getRepository(Spiders::class)->find($id);
+
+        if (!$spider) {
+        throw $this->createNotFoundException(
+            'No spider found for id '.$id
+        );
+    }
+
+
+        $em->remove($spider);
+        $em->flush();
+      /*  $repository = $this->getDoctrine()->getRepository(Spiders::class);
+        $spiders=$repository->findAll();*/
+
+        return $this->redirectToRoute('spider');
+    }
+
+    /**
+ * @Route("/spider/{id}", name="spider_show")
+ */
+public function showAction($id)
+{
+    $spider = $this->getDoctrine()
+        ->getRepository(Spiders::class)
+        ->find($id);
+
+    if (!$spider) {
+        throw $this->createNotFoundException(
+            'No spider found for id '.$id
+        );
+    }
+
+
+    // or render a template
+    // in the template, print things with {{ spider.name }}
+    return $this->render('spider/show.html.twig', ['spider' => $spider]);
+}
+
+    /**
+ * @Route("/spider/update/{id}", name="spider_update")
+ */
+public function updateAction($id, Request $request)
+{
+    $spider = $this->getDoctrine()
+        ->getRepository(Spiders::class)
+        ->find($id);
+
+    if (!$spider) {
+        throw $this->createNotFoundException(
+            'No spider found for id '.$id
+        );
+    }
+
+    $form = $this->createFormBuilder($spider)
+            ->add('Name', TextType::class)
+            ->add('Price', NumberType::class)
+            ->add('Save', SubmitType::class, array('label' => 'Save'))
+            ->getForm();
+
+            $form->handleRequest($request);
+
+   if ($form->isSubmitted() && $form->isValid()) {
+        // $form->getData() holds the submitted values
+        // but, the original `$task` variable has also been updated
+        $spider = $form->getData();
+        $em = $this->getDoctrine()->getManager();
+         $em->persist($spider);
+         $em->flush();
+         return $this->redirectToRoute('spider');
+       }
+
+            return $this->render('spider/update.html.twig', array(
+            'form' => $form->createView(),
+        ));
+}
+}
---
+++ src/Entity/Spiders.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SpidersRepository")
+ */
+class Spiders
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "The name must be at least {{ limit }} characters long",
+     *      maxMessage = "The name cannot be longer than {{ limit }} characters"
+     * )
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2, nullable=true)
+     */
+    private $price;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+}
+
